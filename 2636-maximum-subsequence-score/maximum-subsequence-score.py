22d6a4ff0023d5cb03c45bc8d21a2fffaaead5fa
John <101159322+ChessZra@users.noreply.github.com>
2025-01-03 11:51:45 -0600

    Time: 195 ms (36.47%) | Memory: 42.9 MB (9.71%) - LeetSync

--- 2636-maximum-subsequence-score/maximum-subsequence-score.py
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.py
@@ -2,39 +2,22 @@ class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
         N = len(nums1)
         nums = [(nums1[i], nums2[i]) for i in range(N)]
-        nums = sorted(nums, key=lambda x:x[1])
-
-        pq = [] # max heap
-        for i in range(N):
-            heappush(pq, (-nums[i][0], i))
-
-        lookup = collections.Counter()
-        current_sum = 0
-        for i in range(k - 1):
-            mx, index = heappop(pq)
-            lookup[index] = 1
-            current_sum += -mx
-
-        res = 0
-        #print(nums, current_sum)
-        for i in range(N - k + 1):
-            num1, num2 = nums[i]
+        nums.sort(key=lambda x:-x[1])
         
-            # print(num2, current_sum, num1)
-            if lookup[i] > 0:
-                mx, index = heappop(pq)
-                while index < i:
-                    mx, index = heappop(pq)
-                lookup[index] = 1
-                current_sum += -mx
+        current_sum = 0
 
-                #print('c1:', num2, current_sum, lookup)
-                res = max(res, num2 * current_sum)
-
-                current_sum -= num1
-                lookup[i] = 0
-            else:
-                #print('c2:', num2, current_sum,num1, lookup)
+        pq = [] # min heap of size k - 1
+        res = 0
+        for i in range(N):
+            num1, num2 = nums[i]
+            
+            if len(pq) == k - 1:
                 res = max(res, num2 * (current_sum + num1))
 
+            heappush(pq, num1)
+            current_sum += num1
+
+            if len(pq) == k:
+                current_sum -= heappop(pq)
+                
         return res
