91c88e463493bff0b7bd12013071220ef9309b2e
John <101159322+ChessZra@users.noreply.github.com>
2025-01-07 15:14:06 -0600

    Time: 4 ms (8.66%) | Memory: 18.2 MB (10.65%) - LeetSync

---
+++ 365-water-and-jug-problem/water-and-jug-problem.py
@@ -0,0 +1,37 @@
+class Solution:
+    def canMeasureWater(self, cap_x: int, cap_y: int, target: int) -> bool:
+        
+        def pour(x, y, cap_x, cap_y):
+            dist_to_full = cap_y - y
+            if dist_to_full <= x:
+                return (x - dist_to_full, cap_y)
+            else:
+                return (0, y + x)
+
+        visited = set()
+        @cache
+        def solve(x, y):
+  
+            if (x + y) == target:
+                return 1
+            
+            if x > cap_x or y > cap_y or (x, y) in visited:
+                return 0
+
+            visited.add((x, y))
+
+            best = 0
+            # fill either jug completely
+            best = max(best, solve(cap_x, y), solve(x, cap_y))
+            # empty the thing
+            best = max(best, solve(0, y), solve(x, 0))
+            # pour x
+            new_x, new_y = pour(x, y, cap_x, cap_y)
+            best = max(best, solve(new_x, new_y))
+            # pour y
+            new_x, new_y = pour(y, x, cap_y, cap_x)
+            best = max(best, solve(new_x, new_y))
+
+            return best
+
+        return bool(solve(0, 0))
