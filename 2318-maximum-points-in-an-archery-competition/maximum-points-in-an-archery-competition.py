432739aae0097711777e35978925b12dfc433f53
John <101159322+ChessZra@users.noreply.github.com>
2025-01-08 18:06:17 -0600

    Time: 279 ms (43.48%) | Memory: 34.8 MB (9.78%) - LeetSync

---
+++ 2318-maximum-points-in-an-archery-competition/maximum-points-in-an-archery-competition.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maximumBobPoints(self, numArrows: int, aliceArrows: List[int]) -> List[int]:
+        INF = 10 ** 20
+
+        @cache
+        def solve(arrows_left, index):
+            if index < 0:
+                if arrows_left < 0:
+                    return -INF, tuple()
+                else:
+                    return 0, tuple()
+
+            take, path_take = solve(arrows_left - (aliceArrows[index] + 1), index - 1)
+            dont, path_dont = solve(arrows_left, index - 1)
+
+            path_take = list(path_take)
+            path_take.append(index)
+            take += index
+
+            if take >= dont:
+                return take, tuple(path_take)
+            
+            return dont, path_dont
+
+        res = [0] * 12
+        _, arr = solve(numArrows, 11)
+
+        for index in arr:
+            res[index] = aliceArrows[index] + 1
+
+        sm = sum(res)
+        if sm < numArrows:
+            res[0] += numArrows - sm
+        return res
