84c6e5cefea2c01693a4cb6c16ed8dce1d44d6c6
John <101159322+ChessZra@users.noreply.github.com>
2025-01-17 16:09:41 -0600

    Time: 9 ms (81.60%) | Memory: 18 MB (33.07%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.py
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.py
@@ -1,14 +1,20 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
+        mp = {}
         N = len(nums)
         nu = len(set(nums))
         res = 0
-        for i in range(N):
-            st = set()
-            for j in range(i, N):
-                st.add(nums[j])
+        l = 0
+        for r in range(N):
 
-                if len(st) == nu:
-                    res += N - j
-                    break
+            mp[nums[r]] = 1 + mp.get(nums[r], 0)
+
+            while len(mp) == nu:
+                res += N - r
+                mp[nums[l]] -= 1
+                if mp[nums[l]] == 0:
+                    del mp[nums[l]]
+                l += 1
+                
         return res
+
