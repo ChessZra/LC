b3d3af811cf0ea27deab17f17f27a7935b7c538a
John <101159322+ChessZra@users.noreply.github.com>
2025-01-17 19:26:39 -0600

    Time: 636 ms (19.52%) | Memory: 105.6 MB (5.15%) - LeetSync

--- 2175-count-nodes-with-the-highest-score/count-nodes-with-the-highest-score.py
+++ 2175-count-nodes-with-the-highest-score/count-nodes-with-the-highest-score.py
@@ -1,39 +1,35 @@
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None 
-
 class Solution:
     def countHighestScoreNodes(self, parents: List[int]) -> int:
         N = len(parents)
-        roots = [Node(x) for x in range(N)]
+        adj = [[] for _ in range(N)]
 
         # Create the tree from parents
-        for child in range(1, N):
-            parent = roots[parents[child]]
-            if parent.left is None:
-                parent.left = roots[child]
-            else:
-                parent.right = roots[child]
-        
+        for i in range(1, N):
+            adj[parents[i]].append(i)
+
         @cache
         def get_size(node):
-            if node is None:
-                return 0
-            return 1 + get_size(node.left) + get_size(node.right)
+            sz = 1
+            for c in adj[node]:
+                sz += get_size(c)
+            return sz
 
         f = collections.Counter() # score: frequency
         def dfs(node):
             if node is None:
                 return
             
-            l, r = get_size(node.left), get_size(node.right)
-            p = N - r - l - 1
-            f[max(1, l) * max(1, r) * max(1, p)] += 1
-            dfs(node.left)
-            dfs(node.right)
-
-        dfs(roots[0])
+            prod = 1
+            total = 0
+            for c in adj[node]:
+                total += get_size(c)
+                prod *= get_size(c)
+                dfs(c) # try out other stuff
+                
+            prod *= max(1, N - 1 - total)
+            f[prod] += 1
+            
+            
+        dfs(0)
 
         return f[max(f.keys())]
