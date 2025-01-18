0ae63aaf8867b6135516516f855208edd050900c
John <101159322+ChessZra@users.noreply.github.com>
2025-01-17 20:07:57 -0600

    Time: 15 ms (79.40%) | Memory: 17.9 MB (40.62%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.py
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.py
@@ -1,12 +1,28 @@
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
-        INF = 10 ** 20
         N = len(nums)
         res = 0
+
+        stk = []
         for i in range(N):
-            mn, mx = INF, -INF
-            for j in range(i, N):
-                mn = min(mn, nums[j])
-                mx = max(mx, nums[j])
-                res += (mx - mn)
+
+            while stk and stk[-1][0] <= nums[i]:
+                stk.pop()
+            prev = -1 if not stk else stk[-1][1]
+            prev_sum = 0 if not stk else stk[-1][2]
+            cur_sum = nums[i] * (i - prev) + prev_sum
+            res += cur_sum
+            stk.append((nums[i], i, cur_sum))
+        
+        stk = []
+        for i in range(N):
+
+            while stk and stk[-1][0] >= nums[i]:
+                stk.pop()
+            prev = -1 if not stk else stk[-1][1]
+            prev_sum = 0 if not stk else stk[-1][2]
+            cur_sum = nums[i] * (i - prev) + prev_sum
+            res -= cur_sum
+            stk.append((nums[i], i, cur_sum))
+
         return res
