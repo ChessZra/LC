997b550726bee95efa939f2b7a2b5630b94590d0
John <101159322+ChessZra@users.noreply.github.com>
2025-01-25 16:32:12 -0600

    Time: 1996 ms (25.56%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,8 +1,4 @@
-select d.contest_id, round(count(*) / j.total_users * 100, 2) as percentage
+select d.contest_id, round(count(*) / (select COUNT(*) from Users) * 100, 2) as percentage
 from (select distinct * from Register) d
-cross join (
-    select COUNT(*) as total_users
-    from Users
-) j 
 group by d.contest_id
 order by percentage desc, contest_id
