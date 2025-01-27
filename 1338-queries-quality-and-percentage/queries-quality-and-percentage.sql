9673c9cb39f7bef3aafec8f1cbf17bff7f7e0a99
John <101159322+ChessZra@users.noreply.github.com>
2025-01-26 18:09:55 -0600

    Time: 376 ms (84.53%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,11 +1,5 @@
 select q.query_name, 
     ifnull(round(sum(q.rating / q.position) / count(*), 2), 0) as quality,
-    ifnull(round(total_less_than_3 / count(*) * 100, 2), 0) as poor_query_percentage
+    ifnull(round(sum(case when rating < 3 then 1 else 0 end) / count(*) * 100, 2), 0) as poor_query_percentage
 from Queries q
-left join (
-    select query_name, COUNT(*) as total_less_than_3
-    from Queries
-    where rating < 3
-    group by query_name
-) q2 on q.query_name = q2.query_name
 group by q.query_name
