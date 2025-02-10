0b0f3f2cc0f1720db35e8fb86f45a354932c2f7a
John <101159322+ChessZra@users.noreply.github.com>
2025-02-10 14:07:06 -0600

    Time: 1 ms (31.88%) | Memory: 17.8 MB (64.13%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.py
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.py
@@ -1,30 +1,27 @@
 class Solution:
     def matrixScore(self, grid: List[List[int]]) -> int:
         R, C = len(grid), len(grid[0])
-        while True:
-            changed = False
-            # try to flip all the rows
-            for r in range(R):
-                if grid[r][0] == 0:
-                    for c in range(C):
-                        grid[r][c] = 1 - grid[r][c]
-                    changed = True
 
-            # try to flip all the cols
-            for c in range(C):
-                z = 0
+        changed = False
+        # try to flip all the rows
+        for r in range(R):
+            if grid[r][0] == 0:
+                for c in range(C):
+                    grid[r][c] = 1 - grid[r][c]
+                changed = True
+
+        # try to flip all the cols
+        for c in range(C):
+            z = 0
+            for r in range(R):
+                if grid[r][c] == 0:
+                    z += 1
+            
+            if z > (R - z):
                 for r in range(R):
-                    if grid[r][c] == 0:
-                        z += 1
-                
-                if z > (R - z):
-                    for r in range(R):
-                        grid[r][c] = 1 - grid[r][c]
-                        changed = True
-                
-            if not changed:
-                break
-                
+                    grid[r][c] = 1 - grid[r][c]
+                    changed = True
+            
         res = 0
         for r in range(R):
             i = 0
