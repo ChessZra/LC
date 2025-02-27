15cb715254ec3432ab5b98a253f6c01b76a354db
John <101159322+ChessZra@users.noreply.github.com>
2025-02-27 00:39:37 -0600

    Time: 341 ms (59.58%) | Memory: 42.6 MB (33.80%) - LeetSync

--- 1533-display-table-of-food-orders-in-a-restaurant/display-table-of-food-orders-in-a-restaurant.py
+++ 1533-display-table-of-food-orders-in-a-restaurant/display-table-of-food-orders-in-a-restaurant.py
@@ -1,18 +1,14 @@
 class Solution:
     def displayTable(self, orders: List[List[str]]) -> List[List[str]]:
-        foods = set()
-        # customers = set()
-        tables = set()
 
-        mp = {} # int: collections.Counter
+        foods, tables, mp = set(), set(), dict() 
         for customer_name, table_number, food_item in orders:
-            if table_number not in mp:
-                mp[table_number] = collections.Counter()
-            mp[table_number][food_item] += 1
-
+            int_table_number = int(table_number)
+            if int_table_number not in mp:
+                mp[int_table_number] = collections.Counter()
+            mp[int_table_number][food_item] += 1
             foods.add(food_item)
-            # customers.add(customer_name)
-            tables.add(int(table_number))
+            tables.add(int_table_number)
 
         tables_sorted = sorted(list(tables))
         foods_sorted = sorted(list(foods))
@@ -23,6 +19,6 @@ class Solution:
         for table in tables_sorted:
             temp = [str(table)]
             for food in foods_sorted:
-                temp.append(str(mp[str(table)][food]))
+                temp.append(str(mp[table][food]))
             res.append(temp)
         return res
