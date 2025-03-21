77318893758a7a2c83e443053c58a33881d0bf6d
John <101159322+ChessZra@users.noreply.github.com>
2025-03-21 00:20:07 -0500

    Time: 1115 ms (95.44%) | Memory: 19.6 MB (79.97%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
@@ -1,34 +1,19 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-        graph = {}
-        for i in range(len(recipes)):
-            for j in range(len(ingredients[i])):
-                if recipes[i] not in graph:
-                    graph[recipes[i]] = []
-                graph[recipes[i]].append(ingredients[i][j])
-
-        supplies_lookup = set(supplies)
-        recipes_lookup = set(recipes)
-        visited = []
-        @cache
-        def can(node):
-            if node in supplies_lookup:
-                return True
-            
-            if node not in recipes_lookup or node in visited:
-                return False
-
-            visited.append(node)
-            for neighbor in graph.get(node, []):
-                if not can(neighbor):
-                    return False
-            visited.pop()
-            return True
         
+        N = len(recipes)
         res = []
-        for rec in recipes:
-            visited.clear()
-            if can(rec):
-                res.append(rec)
+        supplies_lookup = set(supplies)
+        for i in range(N):
+            
+            for j in range(N):
+                valid = True
+                for ing in ingredients[j]:
+                    if ing not in supplies_lookup:
+                        valid = False
+                        break
+                if valid:
+                    res.append(recipes[j])
+                    supplies_lookup.add(recipes[j])
 
-        return res
+        return list(set(res))
