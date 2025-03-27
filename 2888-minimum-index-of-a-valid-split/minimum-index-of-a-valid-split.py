9a6e9d621da57c67c96dae09e54768490cd27d96
John <101159322+ChessZra@users.noreply.github.com>
2025-03-26 23:31:45 -0500

    Time: 111 ms (30.69%) | Memory: 34.4 MB (47.53%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
@@ -1,20 +1,27 @@
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
         N = len(nums)
-        total = collections.Counter()
-        for num in nums:
-            total[num] += 1
-        
-        running_heap = []
         f = collections.Counter()
-        for i in range(N - 1):
-            f[nums[i]] += 1
-            heappush(running_heap, (-f[nums[i]], nums[i]))
-
-            freq, num = heappop(running_heap)
-            right = total[num] - f[num]
-            # print(freq, num, f[num], right)
-            if f[num] > ((i + 1) // 2) and right > ((N - i - 1) // 2):
-                return i
+        for num in nums:
+            f[num] += 1
         
+        dominant = None
+        for key, value in f.items():
+            if value > (N // 2):
+                dominant = key
+                break
+        
+        if dominant is None:
+            return -1
+
+        freq = 0
+        for i in range(N):
+            if nums[i] == dominant:
+                freq += 1
+            
+            right = f[dominant] - freq
+
+            if freq > ((i + 1) // 2) and right > ((N - i - 1) // 2):
+                return i
+                
         return -1
