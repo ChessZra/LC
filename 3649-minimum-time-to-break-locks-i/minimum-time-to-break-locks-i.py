30d324b4e05cdb7254ea755d74975f3fed80f12d
John <101159322+ChessZra@users.noreply.github.com>
2025-05-25 23:04:46 -0500

    Time: 99 ms (81.88%) | Memory: 18.6 MB (30.00%) - LeetSync

--- 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.py
+++ 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.py
@@ -1,5 +1,6 @@
 class Solution:
     def findMinimumTime(self, strength: List[int], k: int) -> int:
+        # Bruteforce DP
         N = len(strength)
         target = 2 ** N - 1
 
@@ -16,3 +17,29 @@ class Solution:
             return best
         
         return solve(0, 1)
+        """
+        Greedy attempt - 758/763 cases passed
+        energy = 0
+        x = 1
+        time = 0
+
+        while len(strength) > 0:
+            time_needed_to_wait = math.ceil((min(strength) - energy) / x)
+            # print(strength, x)
+            time += time_needed_to_wait
+            energy += time_needed_to_wait * x
+
+            best = 10 ** 20
+            idx = 0
+            for i in range(len(strength)):
+                if energy >= strength[i] and (energy - strength[i]) < best: 
+                    best = energy - strength[i]
+                    idx = i
+
+            energy = 0
+            strength.pop(idx)
+            x += k
+
+        return time
+
+        """
