fcecb3983c8308a8b6b16e2dce7f4def7ad3e4b8
John <101159322+ChessZra@users.noreply.github.com>
2025-08-19 00:09:49 -0500

    Time: 0 ms (100.00%) | Memory: 21.5 MB (5.03%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,25 +1,24 @@
+def _precompute():
+    arr = []
+    for i in range(0, 10000):
+        y = 3 ** i
+        if y > 10 ** 7:
+            break
+        arr.append(y)
+    all_possible = []
+    path = []
+    N = len(arr)
+    def dfs(index, total_sum):
+        all_possible.append(total_sum)
+        if index >= N:
+            return 
+        dfs(index + 1, total_sum)
+        dfs(index + 1, total_sum + arr[index])
+    dfs(0, 0)
+    return set(all_possible)
+all_possible = _precompute()
+
 class Solution:
-    def __init__(self):
-        self.all_possible = self._precompute()
-    
     def checkPowersOfThree(self, n: int) -> bool:
-        return n in self.all_possible
+        return n in all_possible
     
-    def _precompute(self):
-        arr = []
-        for i in range(0, 10000):
-            y = 3 ** i
-            if y > 10 ** 7:
-                break
-            arr.append(y)
-        all_possible = []
-        path = []
-        N = len(arr)
-        def dfs(index, total_sum):
-            all_possible.append(total_sum)
-            if index >= N:
-                return 
-            dfs(index + 1, total_sum)
-            dfs(index + 1, total_sum + arr[index])
-        dfs(0, 0)
-        return set(all_possible)
