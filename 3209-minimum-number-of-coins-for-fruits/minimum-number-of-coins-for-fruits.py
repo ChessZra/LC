75b0a4ed5bd15dca58b657831ba762d3f3fa2bfe
John <101159322+ChessZra@users.noreply.github.com>
2025-08-19 01:54:17 -0500

    Time: 1108 ms (23.70%) | Memory: 20.6 MB (23.70%) - LeetSync

---
+++ 3209-minimum-number-of-coins-for-fruits/minimum-number-of-coins-for-fruits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumCoins(self, prices: List[int]) -> int:
+        N = len(prices)
+        @cache
+        def solve(index):
+            if index >= N:
+                return 0
+            best = 10 ** 20
+            # print(index, f'iterating from {index + 1} to {index + index + 1}')
+            for i in range(index + 1, index + index + 2):
+                best = min(best, solve(i + 1), solve(i))
+            return best + prices[index]
+        return solve(0)
