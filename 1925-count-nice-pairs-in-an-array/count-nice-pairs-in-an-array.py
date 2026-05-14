140166c3e5ad0aee744fcf8d464f3d1d6870c483
John Ezra <101159322+ChessZra@users.noreply.github.com>
2026-05-14 16:35:19 -0500

    Time: 175 ms (35.11%) | Memory: 28.2 MB (58.43%) - LeetSync

--- 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.py
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.py
@@ -2,14 +2,12 @@ class Solution:
     def countNicePairs(self, nums: List[int]) -> int:
         MOD = 10 ** 9 + 7
         N = len(nums)
-        revs = {}
-        for i in range(N):
-            revs[nums[i]] = int(str(nums[i])[::-1])
         f = collections.Counter()
         res = 0
         for i in range(N):
             # 97 + 24 = 79 + 42
-            res += f[nums[i] - revs[nums[i]]]
+            rev = int(str(nums[i])[::-1])
+            res += f[nums[i] - rev]
             res %= MOD
-            f[nums[i] - revs[nums[i]]] += 1
+            f[nums[i] - rev] += 1
         return res
